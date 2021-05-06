97dd3baf48865c80c0ce3a8d0d4c2382ea2b2222
ph1001 <54173557+ph1001@users.noreply.github.com>
2021-05-06 23:13:54 +0100

    Some testing

---
+++ Philipp/2021-05-03_1624_my_tests/snake_evolution.py
@@ -0,0 +1,26 @@
+# -*- codingng: utf-8 -*-
+"""
+Created on Fri Apr 30 10:39:58 2021
+
+@author: utente
+"""
+
+from call_snake import Population
+from snake_crossover import arithmetic_co
+from snake_mutation import geometric_mutation, normal_distribution_mutation
+from snake_selection import fps, tournament, ranking
+
+snakes = Population(size = 10, 
+                    moves_till_stuck = 300)
+
+snakes.evolve(  gens = 10000, #Number of generations to be produced
+                select = tournament, #Selection function
+                crossover = arithmetic_co, #Crossover function
+                mutate = geometric_mutation, #Mutation function
+                co_p = 0.5, #crossover probability
+                mu_p = 0.5, #mutation probability
+                constant_ms = 7, #Geometric Mutation coefficient 
+                tournament_size = 4,
+                elitism = True, #wheter to perform elitisim
+                record_diversity = True, #wheter to record diversity
+                fitness_sharing = False)
