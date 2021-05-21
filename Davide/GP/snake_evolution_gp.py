88b24f4c9cf934933af40b9dc067351dd8a56cc7
Fari98 <71496424+Fari98@users.noreply.github.com>
2021-05-21 13:00:18 +0100

    gp

--- Davide/GP/snake_evolution_gp.py
+++ Davide/GP/snake_evolution_gp.py
@@ -15,14 +15,14 @@ import math
 
 ## WITH THIS CONFIG APPROX 17h
 
-RUN_NUM = 3 #number of runs to do for each config 
+RUN_NUM = 30 #number of runs to do for each config 
 for run in range(RUN_NUM):
     
     snakes = Population(size = 25, 
                     moves_till_stuck = 100,
-                    fitness_function = lambda x,y: x*y,
+                    fitness_function = lambda x,y: x*math.exp(y) + 101*y,
                     impose_depth = False,
-                    max_depth = 100
+                    max_depth = 200 #MAX 200
                     )
     
     snakes.evolve(  gens = 100, #Number of generations to be produced
@@ -30,12 +30,12 @@ for run in range(RUN_NUM):
                     crossover = gp_co, #Crossover function
                     mutate = gp_mutation, #Mutation function
                     co_p = 0.5, #crossover probability
-                    mu_p = 0.2, #mutation probability
+                    mu_p = 0.5, #mutation probability
                     multi_objective = False, #wheter to perform multiobjective optimization (fitness has to be a tuple)
                     tournament_size = 5, #size of the sample for the tournament selection
                     elitism = True, #wheter to perform elitisim 
                     record_diversity = False, #wheter to record diversity
                     fitness_sharing = False) #wheter to perform fitness sharing
 
-    snakes.log_bestfit(config_name = 'gp_davide', run_number = run)
+    snakes.log_bestfit(config_name = 'gp_davide_200', run_number = run)
     # snakes.log_diversity(config_name = , run_number = run)
